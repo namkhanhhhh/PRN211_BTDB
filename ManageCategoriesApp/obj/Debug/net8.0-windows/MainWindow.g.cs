9c4aab8ff23d59a258cea9d9423a6fd621bc948b
namkhanhhhh <khanhnguyennam04@gmail.com>
2024-06-05 21:16:39 +0700

    Update

--- ManageCategoriesApp/obj/Debug/net8.0-windows/MainWindow.g.cs
+++ ManageCategoriesApp/obj/Debug/net8.0-windows/MainWindow.g.cs
@@ -125,7 +125,7 @@ namespace ManageCategoriesApp {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ManageCategoriesApp;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ManageCategoriesApp;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
